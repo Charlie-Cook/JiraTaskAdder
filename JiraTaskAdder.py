dae4784fba01cd3d03762406fde117554b155851
Charlie Cook <charles.cook@digital.hmrc.gov.uk>
2015-12-11 11:35:49 +0000

    Added feature to retrieve summary automatically.

--- JiraTaskAdder.py
+++ JiraTaskAdder.py
@@ -4,6 +4,7 @@
 
 import requests
 import getpass
+import json
 
 url = "***REMOVED***"
 
@@ -22,52 +23,59 @@ def add_sub_tasks():
     if task_num.isdigit():
         task_num = "AWRS-" + task_num
 
-    while description == "":
-        description = input(
-            "\nPlease enter a description for the sub tickets. \nE.g. Application summary page changes for groups\n\n")
-
-    sub_tasks = [
-        "Dev - ",
-        "Functional Testing - ",
-        "Performance Testing - ",
-        "Accessibility Testing - ",
-        "Penetration Testing - ",
-        "Manual Device Testing - ",
-        "Device Browserstack Testing - "
-    ]
-
-    print(len(sub_tasks))
-
-    for i in range(len(sub_tasks)):
-        payload = {
-            "fields":
-                {
-                    "project":
-                        {
-                            "key": "AWRS"
-                        },
-                    "parent":
-                        {
-                            "key": task_num,
-                        },
-                    "summary": sub_tasks[i] + description,
-                    "description": sub_tasks[i] + description,
-                    "issuetype":
-                        {
-                            "id": "5"
-                        }
-                }
-        }
-
-        j_request = requests.post(url, json=payload, auth=(username, password))
-
-        if j_request.status_code != 201:
-            print("Error with request.")
-
-    while another_ticket != "y" and another_ticket != "Y" and another_ticket != "n" and another_ticket != "N":
-        another_ticket = input("\nWould you like to add another? (y/n)\n")
-
-    if another_ticket == "y" or another_ticket == "Y":
+    t_request = requests.get(url + task_num, auth=(username, password))
+    if t_request.status_code != 200:
+        print("Error getting parent ticket.")
         add_sub_tasks()
+
+    else:
+        parsed_json = json.loads(str(t_request.text))
+        description = parsed_json['fields']['summary']
+
+        if description == "":
+            print("Ticket not found or contains a blank summary.")
+            add_sub_tasks()
+
+        sub_tasks = [
+            "Dev - ",
+            "Functional Testing - ",
+            "Performance Testing - ",
+            "Accessibility Testing - ",
+            "Penetration Testing - ",
+            "Manual Device Testing - ",
+            "Device Browserstack Testing - "
+        ]
+
+        for i in range(len(sub_tasks)):
+            payload = {
+                "fields":
+                    {
+                        "project":
+                            {
+                                "key": "AWRS"
+                            },
+                        "parent":
+                            {
+                                "key": task_num,
+                            },
+                        "summary": sub_tasks[i] + description,
+                        "description": sub_tasks[i] + description,
+                        "issuetype":
+                            {
+                                "id": "5"
+                            }
+                    }
+            }
+
+            j_request = requests.post(url, json=payload, auth=(username, password))
+
+            if j_request.status_code != 201:
+                print("Error creating sub-ticket.")
+
+        while another_ticket != "y" and another_ticket != "Y" and another_ticket != "n" and another_ticket != "N":
+            another_ticket = input("\nWould you like to add another? (y/n)\n")
+
+        if another_ticket == "y" or another_ticket == "Y":
+            add_sub_tasks()
 
 add_sub_tasks()
